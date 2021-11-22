f820b99ab52ef896e746253355fa5e680abdf197
SergioSouz <sergio.dev0208@gmail.com>
2021-11-22 18:58:53 -0300

    finalizando aula LifToff

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import {View,Text, Image } from 'react-native';
+import {View,Text, Image,StatusBar} from 'react-native';
+import IllustrationImg from '../../assets/illustration.png';
 import {style} from './style'; 
+import {ButtonIcon} from "../../components/BottonIcon/index";
 
 
 
 export function SignIn() {
   return (
     <View style={style.container}>
-      <Image source={IllustrationImg} />
+       <StatusBar 
+       barStyle= 'light-content' 
+       backgroundColor= 'transparent'
+       translucent
+       />
+      <Image resizeMode= 'stretch' style ={style.image} source={IllustrationImg}/>
+
+      <View style={style.contant}>
+        <Text style={style.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
+
+        <Text style={style.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+
+        <ButtonIcon 
+        title="Entrar com Discord" 
+        activeOpacity = {0.6} 
+        />
+        
+      </View>
     </View>
   );
 }
--- src/screens/SignIn/style.ts
+++ src/screens/SignIn/style.ts
@@ -1,4 +1,5 @@
 import {StyleSheet} from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 
 export const style = StyleSheet.create({
@@ -6,6 +7,32 @@ export const style = StyleSheet.create({
     flex:1,
     justifyContent:'center',
     alignItems:'center',
+    backgroundColor: theme.color.background,
+   
+
+  },
+  image:{
+    width: '100%',
+    height: 300,
+
+  },
+  contant:{
+    marginTop: -40,
+    paddingHorizontal:50,
+
+  },
+  title:{
+    color: theme.color.heading,
+    textAlign: 'center',
+    fontSize: 40,
+    marginBottom : 16,
+
+  }, 
+  subtitle:{
+    color: theme.color.heading,
+    fontSize: 15,
+    textAlign: 'center',
+    marginBottom: 64,
   },
 
 });
